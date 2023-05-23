a466f7665b1ac7b6beefb96eecfb114a0ab168f5
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-05-22 22:10:41 -0500

    Update challenges

--- oop.rb
+++ oop.rb
@@ -4,23 +4,26 @@
 # it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
 
 class Unicorn
-    def initialize(name1, say)
-        @name1 = name1
-        @color = "silver"
-        @say = say
+    attr_reader :name, :color
+    def initialize(name, color = "silver")
+        @name = name
+        @color = color
     end
-    def say_unicorn
-        "*#{@say}*"
+    def say_unicorn(greeting)
+       puts "*#{greeting}*"
     end
 
 end
 
 
+puts "Unicorn Testing Class"
+unicorn1 = Unicorn.new("Sparkle")
+p unicorn1
+puts unicorn1.name
+puts unicorn1.color
+unicorn1.say_unicorn("Testing, hello!")  ##This method will print the string passed through in the Unicorn1 argument
+puts unicorn1
 
-Unicorn1 = Unicorn.new("Sparkle", "Testing, does 'say' work?")
-p Unicorn1
-Unicorn1.say_unicorn  ##This method will print the string passed through in the Unicorn1 argument
-p Unicorn1
 
 
 #  Write a class called Vampire
@@ -29,6 +32,7 @@ p Unicorn1
 #  it should have a thirsty attribute, that is true by default
 #  it should have a drink method. When called, the thirsty attribute changes to false
 class Vampire
+    attr_reader :name1, :pet, :thirsty
     def initialize(name1, pet = "bat")
         @name1 = name1
         @pet = pet
@@ -39,10 +43,11 @@ class Vampire
     end
 
 end
-Vampire1 = Vampire.new("Carl")
-p Vampire1 #@name1="Carrl", @pet="bat", @thirsty=true>
-Vampire1.drink_method 
-p Vampire1 #Method changes thirsty default: to @name1="Carrl", @pet="bat", @thirsty=false>
+puts "Vampire Test Class"
+vampire1 = Vampire.new("Carl")
+p vampire1 #@name1="Carrl", @pet="bat", @thirsty=true>
+vampire1.drink_method 
+p vampire1 #Method changes thirsty default: to @name1="Carrl", @pet="bat", @thirsty=false>
 
 
 #  Write a Dragon class
@@ -53,32 +58,46 @@ p Vampire1 #Method changes thirsty default: to @name1="Carrl", @pet="bat", @thir
 #  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
 
 class Dragon
-    def initialize(d_name, rider, d_color, times_eat = 2, is_hungry = true)
+    attr_reader :d_name, :rider, :d_color, :times_eat, :is_hungry
+    def initialize(d_name, rider, d_color)
         @d_name = d_name
         @rider = rider
         @d_color = d_color
-        @times_eat = times_eat
-        @is_hungry = is_hungry 
+        @times_eat = 0
+        @is_hungry = true
         
     end
     def eat_method
-        if @times_eat >= 4
-            @is_hungry = false
-        end
+        @times_eat += 1
+        @is_hungry = false if @times_eat >= 4
     end
+    
     
 end
 
-Dragon1 = Dragon.new("Red Hot", "Joseph", "Orange and Red")
-p Dragon1 # Populates as follos: @d_name="Red Hot", @rider="Joseph", @d_color="Orange and Red", @times_eat=2, @is_hungry=true>
+puts "Dragon Class Tests"
+dragon1 = Dragon.new("Red Hot", "Joseph", "Orange and Red")
+puts dragon1.d_name
+puts dragon1.rider
+puts dragon1.d_color
+p dragon1 # Populates as follos: @d_name="Red Hot", @rider="Joseph", @d_color="Orange and Red", @times_eat=2, @is_hungry=true>
+dragon1.eat_method
+puts dragon1.is_hungry
+dragon1.eat_method
+dragon1.eat_method
+dragon1.eat_method
+dragon1.eat_method
+puts dragon1.is_hungry
+p dragon1
 
-Dragon2 = Dragon.new("Billy", "George", "blue", times_eat = 8)
-Dragon2.eat_method
-p Dragon2 # times eaten was changed to 8. Method updates is_hungry to false due to being over 4"Billy", @rider="George", @d_color="blue", @times_eat=8, @is_hungry=false>
+dragon2 = Dragon.new("Billy", "George", "blue")
+dragon2.eat_method
+dragon2.eat_method
+puts dragon2.is_hungry # times eaten was changed to 8. Method updates is_hungry to false due to being over 4"Billy", @rider="George", @d_color="blue", @times_eat=8, @is_hungry=false>
 
-Dragon3 = Dragon.new("Trav", "Ron", "yellow", times_eat = 1)
-Dragon3.eat_method
-p Dragon3 #Verified that the update to times_eat updates is_hungry
+dragon3 = Dragon.new("Trav", "Ron", "yellow")
+dragon3.eat_method
+p dragon3 #Verified that the update to times_eat updates is_hungry
 
 #  Write a Hobbit class
 #  it should have a dynamic name attribute (string)
@@ -90,42 +109,57 @@ p Dragon3 #Verified that the update to times_eat updates is_hungry
 #  it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not, false.
 
 class Hobbit
-    def initialize(h_name, disposition, h_age, is_adult = false, is_old = false, has_ring = false)
+    attr_reader :h_name, :disposition, :h_age
+    def initialize(h_name, disposition, h_age = 0)
         @h_name = h_name
         @disposition = disposition  
         @h_age = h_age
-        @is_adult = is_adult
-        @is_old = is_old
-        @has_ring = has_ring
-     
+        # @is_adult = is_adult
+        # @is_old = is_old
+        # @has_ring = has_ring
+     def is_adult
+        @h_age >= 33
+     end
+     def is_old
+        @h_age >= 101
+     end
+     def celebrate_birthday
+        @h_age += 1
+     end
+     def has_ring
+        @h_name == "Frodo"
+     end
     end
-    def celebrate_birthday
-        @h_age = @h_age + 1
-    end
-    def adult
-        if @h_age >= 101
-            @is_old = true
-            @is_adult = true
-        elsif @h_age >= 33
-            @is_adult = true
-        end
-    def ring_check
-        if @h_name == "Frodo"
-            @has_ring = true
-        end
-    end
-    end
-end 
-Hobbit1 = Hobbit.new("Frodo", "determined", 32)
-p Hobbit1 #h_name="Frodo", @disposition="determined", @h_age=32, @is_adult=false, @is_old=false, @has_ring=false>
-Hobbit1.celebrate_birthday #Method adds 1 to the age to 33
-p Hobbit1
-Hobbit1.adult #Method takes into account the age and redetermines elements 'old' and 'adult'
-p Hobbit1 #Age changed to 33 due to birthday, adult updated to true @h_name="Frodo", @disposition="determined", @h_age=33, @is_adult=true, @is_old=false, @has_ring=false>
-Hobbit1.ring_check #Method checks to see if name is Frodo and changes has_ring to true
-p Hobbit1 #"Frodo", @disposition="determined", @h_age=33, @is_adult=true, @is_old=false, @has_ring=true>
 
-Hobbit2 = Hobbit.new("OldJack", "freindly", 108)
-p Hobbit2 #@h_name="OldJack", @disposition="freindly", @h_age=108, @is_adult=false, @is_old=false, @has_ring=false>
-Hobbit2.adult #Method called to update age booleans based on data
-p Hobbit2 #Method updated both is_adult and is_old based on age - @h_name="OldJack", @disposition="freindly", @h_age=108, @is_adult=true, @is_old=true, @has_ring=false>
+end 
+puts "Hobbit Class Tests"
+hobbit1 = Hobbit.new("Frodo", "determined")
+puts hobbit1.h_name
+puts hobbit1.disposition
+puts hobbit1.h_age
+puts hobbit1.is_adult
+puts hobbit1.is_old
+puts hobbit1.has_ring
+
+34.times { hobbit1.celebrate_birthday}
+puts hobbit1.h_age
+puts hobbit1.is_adult
+puts hobbit1.is_old
+
+101.times {hobbit1.celebrate_birthday}
+puts hobbit1.h_age
+puts hobbit1.is_adult
+puts hobbit1.is_old
+
+p hobbit1 #h_name="Frodo", @disposition="determined", @h_age=32, @is_adult=false, @is_old=false, @has_ring=false>
+hobbit1.celebrate_birthday #Method adds 1 to the age to 33
+p hobbit1
+hobbit1.is_adult #Method takes into account the age and redetermines elements 'old' and 'adult'
+p hobbit1 #Age changed to 33 due to birthday, adult updated to true @h_name="Frodo", @disposition="determined", @h_age=33, @is_adult=true, @is_old=false, @has_ring=false>
+hobbit1.has_ring #Method checks to see if name is Frodo and changes has_ring to true
+p hobbit1 #"Frodo", @disposition="determined", @h_age=33, @is_adult=true, @is_old=false, @has_ring=true>
+
+hobbit2 = Hobbit.new("OldJack", "freindly", 108)
+p hobbit2 #@h_name="OldJack", @disposition="freindly", @h_age=108, @is_adult=false, @is_old=false, @has_ring=false>
+hobbit2.is_adult #Method called to update age booleans based on data
+p hobbit2 #Method updated both is_adult and is_old based on age - @h_name="OldJack", @disposition="freindly", @h_age=108, @is_adult=true, @is_old=true, @has_ring=false>
